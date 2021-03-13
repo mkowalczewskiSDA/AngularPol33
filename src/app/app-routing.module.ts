018b24063b7a8c786bcbeb44d376332c2ec4fc57
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-03-13 12:01:55 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { UserFormComponent } from "./component/user/user-form/user-form.component";
 import { UserListComponent } from "./component/user/user-list/user-list.component";
 
 const routes: Routes = [
-    { path: 'users', component: UserListComponent}
+    { path: 'users', component: UserListComponent},
+    { path: 'addUser', component: UserFormComponent}
 ]
 
 @NgModule({
