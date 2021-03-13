96ec4d25f3cab85e3d7bcabe3ba4ff9dbc029b13
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-03-13 11:17:35 +0100

    commit

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { r } from '@angular/core/src/render3';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../model/user';
 
 @Injectable()
 export class UserService {
 
-  private usersUrl:
+  private usersUrl: string
+  private users: Observable<User[]>
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.usersUrl = 'http://localhost:8080/api/users'
+   }
+
+   public findAll(): Observable<User[]> {
+     this.users = this.http.get<User[]>(this.usersUrl)
+     return this.users
+   }
 
 }
